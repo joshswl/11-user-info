a2eb360f4a78da76e01d927e32d4246110a3b3fa
Josh Lee <joshswl@gmail.com>
2017-02-16 16:05:21 -0600

    first draft

--- app/index.js
+++ app/index.js
@@ -7,3 +7,12 @@ userPhoto.src = data.results[0].picture.large;
 
 const userName = document.querySelector('.space__name');
 userName.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`;
+
+const userEmail = document.querySelector('.space__email');
+userEmail.innerText = data.results[0].email;
+
+const userPhone = document.querySelector('.space__phone');
+userPhone.innerText = data.results[0].phone;
+
+const userLocation = document.querySelector('.space__location');
+userLocation.innerText = `${data.results[0].location.city}, ${data.results[0].location.state}`;
