c549e56ce89907b51ef7032f6cb4fc883a4299f0
Josh Lee <joshswl@gmail.com>
2017-02-16 15:49:12 -0600

    got the first two elements replaced with user-data.js

--- app/index.js
+++ app/index.js
@@ -3,4 +3,7 @@ import 'whatwg-fetch';
 import data from './user-data';
 
 const userPhoto = document.querySelector('.avatar__pic');
-userPhoto.innerText = data.results[0].picture.large;
+userPhoto.src = data.results[0].picture.large;
+
+const userName = document.querySelector('.space__name');
+userName.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`;
